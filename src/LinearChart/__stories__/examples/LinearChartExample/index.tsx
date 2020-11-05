7f24735c9a6b5632c23de8f0ac28ca3a1b9b2660
Anya Ovsyannikova <arhayka@gmail.com>
2020-11-05 11:49:38 +0300

    add example

--- src/LinearChart/__stories__/examples/LinearChartExample/index.tsx
+++ src/LinearChart/__stories__/examples/LinearChartExample/index.tsx
@@ -2,20 +2,47 @@ import React from 'react'
 
 import classnames from 'classnames'
 
-import { BackgroundBarChart } from '../../..'
-import { groups } from '../../../data.mock'
+import { axes, directionsX, directionsY, LinearChart } from '../../..'
 
 import css from './index.css'
 
 export const LinearChartExample = () => (
   <div className={classnames(css.main)}>
     <LinearChart
-      groups={groups}
-      gridTicks={4}
-      valuesTicks={1}
-      isHorizontal={true}
-      showValues={false}
-      align="start"
+      title="Очень красивый график"
+      lines={[
+        {
+          values: [
+            { x: 1, y: 1 },
+            { x: 5, y: 3 },
+            { x: 7, y: 1 },
+            { x: 8, y: 2 },
+          ],
+          dots: true,
+          lineName: 'Северный бур',
+          withGradient: true,
+          color: 'var(--color-bg-success)',
+        },
+      ]}
+      gridConfig={{
+        x: {
+          labels: 'bottom',
+          labelTicks: 1,
+          gridTicks: 10,
+          guide: true,
+          withPaddings: false,
+        },
+        y: {
+          labels: 'left',
+          labelTicks: 1,
+          gridTicks: 4,
+          guide: true,
+          withPaddings: false,
+        },
+      }}
+      isHorizontal
+      units="км"
+      background="linear-gradient(to right, #f54d4d48, transparent)"
     />
   </div>
 )
